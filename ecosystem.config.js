b4661580c3eac3c0b7fa8ccddcb1368ddc5609dc
4rt3n51 <artenis.islami@fshnstudent.info>
2026-02-20 12:56:04 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'phoneshop',
-      script: 'server.js',
-      // If you keep env vars in the instance environment, you can omit or set them here:
+      name: "phoneshop",
+      script: "server.js",
+      env: {
+        NODE_ENV: "development"
+      },
       env_production: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
+        DB_HOST: "database-1.cctawceo4pkq.us-east-1.rds.amazonaws.com",
+        DB_USER: "admin",
+        DB_PASSWORD: "F3bruary!26",
+        DB_NAME: "PhoneShop",
         PORT: 3000
       }
     }
   ]
 };
-env_production: {
-  DB_HOST: "database-1.cctawceo4pkq.us-east-1.rds.amazonaws.com",
-  DB_USER: "admin",
-  DB_PASSWORD: "F3bruary!26",
-  DB_NAME: "PhoneShop",
-  NODE_ENV: "production",
-  PORT: 3000
-}
