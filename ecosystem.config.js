9d74757ba2511f6f6f52530fa6227baa987ca87d
4rt3n51 <artenis.islami@fshnstudent.info>
2026-02-23 11:16:09 +0100

    Fix script path in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,6 @@
 module.exports = {
-  apps: [{
-    name: 'project-phoneshop',
-    script: './src/server.js',
-    env: {
-      NODE_ENV: 'production',
-      PORT: process.env.PORT || 3000,
-      DB_HOST: process.env.DB_HOST || 'localhost',
-      DB_USER: process.env.DB_USER || 'user',
-      DB_PASS: process.env.DB_PASS || 'pass',
-      DB_NAME: process.env.DB_NAME || 'mydatabase',
-    },
-    instances: 'max',
-    exec_mode: 'cluster',
-    watch: false,
-    log_file: 'logs/combined.outer.log',
-    out_file: 'logs/combined.out.log',
-    error_file: 'logs/combined.error.log',
-    time: true,
-  }]
+  apps : [{
+    name   : "project-phoneshop",
+    script : "./server.js"
+  }],
 };
